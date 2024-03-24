9bad8000f9e126ad051b064a0d5d78e0605cb10c
Mariana Pereira <marianapereira.s@live.com>
2024-03-24 04:11:55 -0300

    Testes para a classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,32 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from tests.ingredients import INGREDIENTS
 
 
-# Req 1 iniciando
+# Req 1
 def test_ingredient():
-    pass
+    ingredient_mock = INGREDIENTS[0]
+    ingredient = Ingredient(ingredient_mock.name)
+
+    # falha caso o atributo name de um ingrediente seja diferente que o passado ao construtor
+    assert ingredient.name == ingredient_mock.name
+
+    # falha caso o atributo restrictions de um ingrediente não contenha os valores corretos para o alimento passado
+    assert ingredient.restrictions == ingredient_mock.restrictions
+
+    #  falha caso a implementação do método __repr__ retorne um valor inadequado
+    assert ingredient.__repr__() == f"Ingredient('{ingredient_mock.name}')"
+
+    # falha caso a classe retorne hashes diferentes para dois ingredientes iguais
+    assert ingredient.__hash__() == ingredient.__hash__()
+
+    # falha caso a comparação de igualdade de dois ingredientes iguais (ou de um ingrediente com ele mesmo) seja falsa
+    assert ingredient == ingredient
+
+    another_ingredient_mock = INGREDIENTS[1]
+    another_ingredient = Ingredient(another_ingredient_mock.name)
+
+    # falha caso a classe retorne hashes iguais para dois ingredientes diferentes
+    assert ingredient.__hash__() != another_ingredient.__hash__()
+
+    # falha caso a comparação de igualdade de dois ingredientes diferentes seja verdadeira
+    assert ingredient != another_ingredient
